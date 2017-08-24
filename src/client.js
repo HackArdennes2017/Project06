c9831198488816331475db82a10626100dbafa38
meriadec <meriadec.pillet@gmail.com>
2017-08-24 21:51:27 +0200

    init

---
+++ src/client.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { ConnectedRouter } from 'react-router-redux'
+import createHistory from 'history/createBrowserHistory'
+import { AppContainer } from 'react-hot-loader'
+
+import createStore from 'store'
+
+import App from 'components/App'
+
+import 'styles/main.scss'
+
+const history = createHistory()
+const store = createStore(history, window.__INITIAL_STATE__)
+
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      {Component(store, ConnectedRouter, { history })}
+    </AppContainer>,
+    document.getElementById('root'),
+  )
+}
+
+render(App)
+
+if (module.hot) {
+  module.hot.accept('components/App', () => {
+    const nextApp = require('components/App')
+    render(nextApp)
+  })
+}
---
+++ webpack/dev.js
@@ -0,0 +1,24 @@
+import webpack from 'webpack'
+
+import webpackConfig from './base'
+
+export default {
+  ...webpackConfig,
+  devtool: 'eval',
+  entry: ['react-hot-loader/patch', ...webpackConfig.entry, 'webpack-hot-middleware/client'],
+  module: {
+    rules: [
+      ...webpackConfig.module.rules,
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'autoprefixer-loader'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    ...webpackConfig.plugins,
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+  ],
+}
