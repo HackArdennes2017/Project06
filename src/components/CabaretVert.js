e42a355311f5f039eaffe3093bbe8634b6e8e95b
Yann Pringault <yann.pringault@gmail.com>
2017-08-25 19:29:37 +0200

    Add button to give page

--- src/components/CabaretVert.js
+++ src/components/CabaretVert.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react'
+import { push } from 'react-router-redux'
+import { connect } from 'react-redux'
 
 import Layout from 'components/Layout'
+import Button from 'components/Button'
 
+@connect(null, { push })
 class CabaretVert extends Component {
   render() {
     return (
-      <Layout className="justify-between px2" title="Cabaret Vert">
-
+      <Layout title="Cabaret Vert">
+        <div className="px2 py3">
+          <Button onClick={() => this.props.push('/cabaret-vert/give')}>
+            Je donne
+          </Button>
+          <div className="flex-row items-center my2">
+            <div
+              className="flex-auto"
+              style={{ height: 1, backgroundColor: '#ccc' }}
+            />
+            <div className="mx2">ou</div>
+            <div
+              className="flex-auto"
+              style={{ height: 1, backgroundColor: '#ccc' }}
+            />
+          </div>
+        </div>
       </Layout>
     )
   }
