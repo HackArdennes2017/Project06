5c0d547d1ab8b99bbf973ae2a137811db36951d1
meriadec <meriadec.pillet@gmail.com>
2017-08-26 09:39:05 +0200

    oops

--- src/server/resizeImage.js
+++ src/server/resizeImage.js
@@ -5,11 +5,14 @@ const UPS = path.resolve(__dirname, '../assets/uploads')
 
 export default async function resizeImage(filename) {
   try {
+    // LOL. what a shitty block of code
     const fullFilename = path.resolve(UPS, filename)
     const extension = path.extname(fullFilename)
-    const withoutExt = fullFilename.substr(0, fullFilename.lastIndexOf('.'))
+    const withoutExtFull = fullFilename.substr(0, fullFilename.lastIndexOf('.'))
+    const resizeFilenameFull = `${withoutExtFull}_small${extension}`
+    const withoutExt = filename.substr(0, filename.lastIndexOf('.'))
     const resizeFilename = `${withoutExt}_small${extension}`
-    await sharp(fullFilename).resize(200, 200).toFile(resizeFilename)
+    await sharp(fullFilename).resize(200, 200).toFile(resizeFilenameFull)
     return resizeFilename
   } catch (e) {
     return ''
