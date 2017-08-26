86c053300c0dcbc5d220d3ffac16e967e313d298
Yann Pringault <yann.pringault@gmail.com>
2017-08-26 22:38:31 +0200

    Gravity #Insider #FrancoisMallet

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -7,23 +7,49 @@ import Layout from 'components/Layout'
 
 import 'styles/welcome.scss'
 
+const LogoIcon = ({ style, icon }) =>
+  <div className="logo-icons" style={style}>
+    <div className="translate">
+      <img src={`/assets/logo_icons/${icon}.svg`} className="scale" />
+    </div>
+  </div>
+
 @connect(null, { push })
 class Welcome extends Component {
-  state = {
-    isLoading: false,
-  }
+  state = { isLoading: false }
+
   handleContinue = () => {
     this.setState({ isLoading: true })
     setTimeout(() => {
       this.props.push('/festivals')
     }, 800)
   }
+
   render() {
     const { isLoading } = this.state
     return (
       <Layout withNavBar={false} className="flex-auto items-center p3">
-        <div className="justify-center items-center" style={{ marginTop: 70 }}>
-          <img alt="logo" height={170} src="/assets/logo.png" />
+        <div
+          className="justify-center items-center relative"
+          style={{ marginTop: 60 }}
+        >
+          <div className="logo-icons-wrapper" style={{ opacity: 0.6 }}>
+            <LogoIcon icon="chair" style={{ top: 0, left: 10 }} />
+            <LogoIcon icon="tent" style={{ top: -35, left: 70 }} />
+            <LogoIcon icon="mattress" style={{ top: 0, right: 0 }} />
+            <LogoIcon icon="sleeping-bag" style={{ top: 55, left: -20 }} />
+            <LogoIcon icon="chair" style={{ bottom: -20, left: 65 }} />
+            <LogoIcon icon="tent" style={{ bottom: 0, left: 0 }} />
+            <LogoIcon icon="sleeping-bag" style={{ bottom: 0, right: 0 }} />
+            <LogoIcon icon="tent" style={{ top: 55, right: -17 }} />
+          </div>
+          <img
+            className="relative"
+            alt="logo"
+            height={170}
+            src="/assets/logo.png"
+            style={{ zIndex: 10 }}
+          />
         </div>
         <div
           onClick={() => this.props.push('/about')}
@@ -36,12 +62,12 @@ class Welcome extends Component {
           {'Comment ça marche ?'}
         </div>
         <div
-          className="mt3"
+          className="mt4"
           style={{ fontSize: 30, textAlign: 'center', fontWeight: 'bold' }}
         >
           Les Sardines
         </div>
-        <div className="mt3" style={{ width: 270, textAlign: 'center' }}>
+        <div className="mt2" style={{ width: 270, textAlign: 'center' }}>
           La plateforme solidaire de matériel de camping pour festivaliers
         </div>
         <Button
