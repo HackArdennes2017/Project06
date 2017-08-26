99956af5a8d59a687d89dfdee5bdccbd8228e247
Yann Pringault <yann.pringault@gmail.com>
2017-08-26 08:36:52 +0200

    Add some helper texts

--- src/components/CabaretVert.js
+++ src/components/CabaretVert.js
@@ -21,26 +21,28 @@ class CabaretVert extends Component {
     return (
       <Layout title="Cabaret Vert" backRoute="/festivals">
         <div className="px3 pt3">
+          <div className="mb1 center">J&apos;aimerai donner du matériel</div>
           <Button
             onClick={() => this.props.push('/cabaret-vert/give')}
             style={{ backgroundColor: '#fc7772' }}
           >
-            Je donne
+            Je donne !
           </Button>
           <div className="flex-row items-center my2">
             <div
-              className="flex-auto ml4"
-              style={{ height: 1, backgroundColor: '#ddd' }}
+              className="flex-auto ml3"
+              style={{ height: 1, backgroundColor: '#ccc' }}
             />
             <div className="mx2" style={{ color: '#ccc' }}>
               ou
             </div>
             <div
-              className="flex-auto mr4"
-              style={{ height: 1, backgroundColor: '#ddd' }}
+              className="flex-auto mr3"
+              style={{ height: 1, backgroundColor: '#ccc' }}
             />
           </div>
         </div>
+        <div className="mb1 center">J&apos;ai besoin de matériel</div>
         <div>
           {items.map((item, index) => <Item key={index} item={item} />)}
         </div>
